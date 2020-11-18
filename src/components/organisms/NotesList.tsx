53c0d489e52f7197a719cde63dea8754aff72618
Bodia Valeur <bodiavaleur@Bodias-MacBook-Air.local>
2020-11-18 05:37:21 +0200

    Made component to show list of notes

--- src/components/organisms/NotesList.tsx
+++ src/components/organisms/NotesList.tsx
@@ -1,6 +1,14 @@
 import React from "react";
+import { NotesCollection } from "../../types/NoteTypes";
 import { List } from "../../ui/organisms";
+import { Note } from "../molecules";
 
-export function NotesList() {
-  return <List></List>;
+export function NotesList({ notes }: NotesCollection) {
+  return (
+    <List>
+      {notes.map((note) => (
+        <Note {...note} />
+      ))}
+    </List>
+  );
 }
