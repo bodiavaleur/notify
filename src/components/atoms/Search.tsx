8d012742e8177a45fc414de7d142646c313d920c
Bodia Valeur <bodiavaleur@Bodias-MacBook-Air.local>
2020-12-26 08:44:27 +0200

    Small fixes

--- src/components/atoms/Search.tsx
+++ src/components/atoms/Search.tsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { setSearchQuery } from "../../redux/actions";
 import { SearchInput } from "../../ui/atoms";
 
 export function Search() {
-  return <SearchInput placeholder='Search' />;
+  const dispatch = useDispatch();
+
+  const handleSearch = (evt: React.ChangeEvent<HTMLInputElement>) =>
+    dispatch(setSearchQuery(evt.target.value));
+
+  return <SearchInput placeholder='Search' onChange={handleSearch} />;
 }
--- src/components/organisms/NotesList.tsx
+++ src/components/organisms/NotesList.tsx
@@ -5,13 +5,20 @@ import { List } from "../../ui/organisms";
 import { Note } from "../molecules";
 
 export function NotesList() {
-  const notes = useSelector((state: NotesState) => state.notes);
+  const { notes, searchQuery } = useSelector((state: NotesState) => ({
+    notes: state.notes,
+    searchQuery: state.searchQuery,
+  }));
 
-  return (
-    <List>
-      {notes.map((note) => (
-        <Note {...note} />
-      ))}
-    </List>
-  );
+  const showNotes = () => {
+    let allNotes = [...notes];
+
+    if (searchQuery) {
+      allNotes = allNotes.filter((note) => note.title.includes(searchQuery));
+    }
+
+    return allNotes.map((note) => <Note {...note} />);
+  };
+
+  return <List>{showNotes()}</List>;
 }
